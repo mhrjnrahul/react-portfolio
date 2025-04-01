f65d3f609363f2973fe052025accf3293f3cf67c
mhrjnrahul <maharjanrahul58@gmail.com>
2025-04-01 07:22:09 +0545

    added project

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,7 @@
 import project1 from "../assets/projects/project-1.jpg";
 import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
 
 export const HERO_CONTENT = `Hi, I'm Rahul Maharjan, a passionate Front-End Developer dedicated to crafting modern, responsive, and visually appealing web experiences. With expertise in React and Tailwind CSS, I focus on building intuitive user interfaces that enhance user engagement. My goal is to turn ideas into pixel-perfect designs and seamless interactions, ensuring high performance and accessibility. Whether it's designing sleek landing pages or developing complex web applications, I thrive on creating digital experiences that leave a lasting impact. Let's build something amazing together! 🚀`;
 
@@ -16,13 +17,6 @@ When I'm not coding, you'll find me exploring new design trends. Let's collabora
 
 
 export const PROJECTS = [
-  // {
-  //   title: "E-Commerce Website",
-  //   image: project1,
-  //   description:
-  //     "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-  //   technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
-  // },
   {
     title: "Google Keep clone",
     image: project1,
@@ -43,6 +37,13 @@ export const PROJECTS = [
     description:
       "A simple to-do list application with features like task creation, deletion, and completion tracking.",
     technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+  },
+  {
+    title: "Zentry-clone",
+    image: project4,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
   },
 ];
 
